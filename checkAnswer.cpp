9c33dd06aface1f48ba0b0c5ad52062d4a6e0617
MohammedDigitalSage <159501876+MohammedDigitalSage@users.noreply.github.com>
2025-02-15 22:05:28 -0500

    Update checkAnswer.cpp

--- checkAnswer.cpp
+++ checkAnswer.cpp
@@ -3,6 +3,7 @@
 
 // Mohammed U.
 // CSCI 13500
+// This is a match-making or exact string matching type of question.
 
 #include <iostream>
 #include <string>
@@ -10,17 +11,20 @@
 using namespace std;
 
 int main() {
-    // Define the questions and expected answers
+    // Define the questions and expected answers:
+
+    // Question 1; if they match, we output "True," else we output "False"
     string question1 = "What is -1 + 5 / 3 ?";
     string expected_answer1 = "0";
 
+    // Question 2; if they match, we output "True," else we output "False"
     string question2 = "Assume that n is properly declared and initialized. Write a condition to represent that n does NOT satisfy 0 < n < 100.";
     string expected_answer2 = "(n <= 0 || n >= 100)";
 
     string user_answer;
 
     // First question
-    cout << question1 << endl;
+    cout << question1 << endl; // "What is -1 + 5 / 3 ?"
     cout << "Your Answer: ";
     getline(cin, user_answer);
 
@@ -32,7 +36,7 @@ int main() {
 
     // Second question
     cout << endl;
-    cout << question2 << endl;
+    cout << question2 << endl; // "Assume that n is properly declared and initialized. Write a condition to represent that n does NOT satisfy 0 < n < 100.";
     cout << "Your Answer: ";
     getline(cin, user_answer);
 
